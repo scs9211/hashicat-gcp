39a97e4bd522d9b70b76a0d9df8c826f6a1a9208
Sam S <sam.c.sprouse@ey.com>
2022-03-11 14:44:56 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -3,10 +3,11 @@ module "vpc" {
     version = "~> 4.0"
     network_name = "sam-network"
     project_id = var.project
-   subnets = [
-    {
-     subnet_name   = "gaurav-subnet"
-     subnet_ip     = "10.100.10.0/24"
-     subnet_region = var.region
-    }
-]
+    subnets = [
+   {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+   }
+ ]
+}
