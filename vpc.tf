e432de47c3ded7e519080dc0f7f98d19326d35fe
Sam S <sam.c.sprouse@ey.com>
2022-03-11 14:42:17 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,12 @@
-network_name = "sam-network"
-project_id = var.project
-subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 4.0"
+    network_name = "sam-network"
+    project_id = var.project
+   subnets = [
+    {
+     subnet_name   = "gaurav-subnet"
+     subnet_ip     = "10.100.10.0/24"
+     subnet_region = var.region
+    }
 ]
